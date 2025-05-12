f9460056ca8fe6f1242ecfe70c887f0ac87b06ad
Manal Boulahya <manal.boulahya@student.ehb.be>
2025-05-12 23:55:52 +0200

    about section aangepast + design card

--- cp-frontend/src/components/AboutSection.jsx
+++ cp-frontend/src/components/AboutSection.jsx
@@ -2,16 +2,23 @@ function AboutSection() {
   return (
     <section className="about-section">
       <h2>
-        ABOUT <span>US</span>
+        ER WAS <span>EENS</span>...
       </h2>
       <p>
-        Er was eens... is een verzameling van prachtige sprookjes gemaakt door
-        de studenten van Multimedia & Creatieve Technologie. Elk verhaal is
-        uniek, visueel boeiend en bevat een bijzondere boodschap.
+        Er was eens... In een wereld vol pixels en verbeelding schreven de
+        studenten van Multimedia & Creatieve Technologie hun eigen moderne
+        sprookjes. Geen klassieke prinsen en draken, maar verhalen die raken —
+        vol emotie, fantasie en betekenis. Deze verzameling sprookjes is het
+        resultaat van wekenlang intens creatief werk. Van het eerste flitsende
+        idee, naar schetsen, storyboards en digitale illustraties, tot animaties
+        die bewegen of interactieve ervaringen die je meenemen in een ander
+        universum.
         <br />
         <br />
-        Onze studenten hebben met passie gewerkt aan hun sprookjes: van idee,
-        naar storyboard, illustratie tot animatie of interactie.
+        Elk sprookje is uniek. Soms grappig, soms ontroerend. Soms herkenbaar,
+        soms verrassend vreemd. Maar altijd gemaakt met liefde, zorg en een
+        flinke dosis creativiteit. Welkom in een wereld waar technologie en
+        verbeelding samenkomen. Welkom in onze sprookjes.
       </p>
       <a href="/sprookjes" className="about-button">
         Discover all projects
--- cp-frontend/src/components/SprookjesCard2.jsx
+++ cp-frontend/src/components/SprookjesCard2.jsx
@@ -4,24 +4,20 @@ function SprookjeCard2({ id, title, genre, firstName, lastName, link, image }) {
   return (
     <div className="sprookje-card2">
       <img src={image} alt={title} className="sprookje-img2" />
-      <p>
+
+      <div className="card-row">
         <strong>{title}</strong>
-      </p>
-      <p>
-        {firstName} {lastName}
-      </p>
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          → View story
+        </a>
+      </div>
 
-      <br />
-
-      {/* 👉 lien vers l’histoire en externe */}
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        → View story
-      </a>
-
-      <br />
-
-      {/* 👉 lien interne vers la page makingof */}
-      <Link to={`/makingof/${id}`}>→ Behind the scenes</Link>
+      <div className="card-row">
+        <p>
+          {firstName} {lastName}
+        </p>
+        <Link to={`/makingof/${id}`}>→ Behind the scenes</Link>
+      </div>
     </div>
   );
 }
